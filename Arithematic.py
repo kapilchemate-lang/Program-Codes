9884d73f23f8c9bb23b5d708d3f2ac3549305ade
kapilchemate-lang <kapilchemate@gmail.com>
2026-07-21 18:04:17 +0530

    Add files via upload

---
+++ Arithematic.py
@@ -0,0 +1,28 @@
+print("Program for Arithematic operations")
+a=int(input("Enter a="))
+b=int(input("Enter b="))
+
+#Sum of numbers
+a1=a+b
+
+#Diffrence of numbers
+a2=a-b
+
+#Product of numbers
+a3=a*b
+
+#Division of numbers
+a4=a/b
+
+#Modulus of numbers
+a5=a%b
+
+#Exponent of numbers
+a6=a**b
+
+print(f"The Addition of {a} and {b} is",a1)
+print(f"The subtraction of {a} and {b} is",a2)
+print(f"The Multiplication of {a} and {b} is",a3)
+print(f"The Division of {a} and {b} is",a4)
+print(f"The Modulus of {a} and {b} is",a5)
+print(f"The Exponent of {a} and {b} is",a6)
