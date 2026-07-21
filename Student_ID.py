1120c720cfbd4e3679a15cc739f2cd19881bad46
kapilchemate-lang <kapilchemate@gmail.com>
2026-07-21 18:05:52 +0530

    Add files via upload

---
+++ Student_ID.py
@@ -0,0 +1,7 @@
+print("Code to display information on id card of student")
+a=input("Enter your Name:")
+b=input("Enter your Enrollment Number:")
+c=input("Enter your Department name:")
+d=input("Enter your joining year:")
+
+print(f"Name:{a}\nEnrollment No.{b}\nDept:{c}\nBatch:{d}")
