b18971087bd93bf4f87bd59cba469d7997b39495
kapilchemate-lang <kapilchemate@gmail.com>
2026-08-01 08:53:08 +0530

    Add files via upload

---
+++ Slicing and greatest number program.py
@@ -0,0 +1,22 @@
+print("Slicing program")
+
+#list of numbers
+numbers=[1,2,3,4,5,6,7,8]
+
+#printing every second element of the list
+print(numbers[::2])
+
+number=numbers[::-1]
+print(number)'''
+
+print("program for largest number")
+
+#entering two numbers
+a=int(input("Enter first number 'A':"))
+b=int(input("Enter second number'B':"))
+
+#condition for greatest number
+if(a>b):
+	print("'A' is greater")
+else:
+	print("'B' is greater")
